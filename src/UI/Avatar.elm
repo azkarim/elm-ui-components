93f7e76140340cbb285009772561c385d34f1891
azkarim <karimazizul1@gmail.com>
2024-01-23 01:38:24 +0600

    Avatar component

---
+++ src/UI/Avatar.elm
@@ -0,0 +1,31 @@
+module UI.Avatar exposing (avatar)
+
+import Element exposing (Element)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import UI.Preset.Size as Size
+import UI.Theme exposing (theme)
+
+
+avatar : List (Element.Attribute msg) -> Element msg -> Element msg
+avatar attrs elem =
+    Element.el
+        (Element.width (Element.px square)
+            :: Element.height (Element.px square)
+            :: Border.rounded 50
+            :: Background.color theme.color.secondary
+            :: Font.size Size.spacing3
+            :: attrs
+        )
+    <|
+        Element.el
+            [ Element.centerX
+            , Element.centerY
+            ]
+            elem
+
+
+square : Int
+square =
+    Size.spacing10
